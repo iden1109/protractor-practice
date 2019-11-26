be535f01679bb5412d28d4056919935a45f517de
Iden <iden1109@gmail.com>
2019-11-26 23:29:42 +0800

    test case rename for readability

--- e2e/src/super-calculator.e2e-spec.ts
+++ e2e/src/super-calculator.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { browser, by, element, $, $$, ExpectedConditions as EC } from 'protractor';
 
-describe('Super Calculator', () => {
+describe('Test Super Calculator', () => {
   let first = element(by.model('first'));
   let second = element(by.model('second'));
   let operator = element(by.model('operator'));
@@ -16,14 +16,14 @@ describe('Super Calculator', () => {
     expect(await browser.getTitle()).toEqual('Super Calculator');
   });
 
-  it('should add one and two', async () => {
+  it('should add two numbers one and two', async () => {
     await first.sendKeys(1);
     await second.sendKeys(2);
     await goButton.click();
     expect(await lastResult.getText()).toEqual('3');
   });
 
-  it('should add four and six', async () => {
+  it('should add two nunmbers four and six', async () => {
     await first.sendKeys(4);
     await second.sendKeys(6);
     await goButton.click();
