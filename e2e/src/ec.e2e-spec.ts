bc4761f7d8945ffd76b6ad739177d6d2eda49b0c
Nathan Lin 林昇暉 (nathanlin) <nathanlin@micron.com>
2019-12-12 10:36:43 +0800

    Modify expect condition

--- e2e/src/ec.e2e-spec.ts
+++ e2e/src/ec.e2e-spec.ts
@@ -7,7 +7,7 @@ describe('the user does search on angular.io', () => {
         await browser.wait(EC.presenceOf(element(by.css('body > aio-shell > aio-search-results > div > div:nth-child(2)'))), 5000);
 
         await element(by.css('body > aio-shell > aio-search-results > div > div:nth-child(2) > ul.priority-pages > li:nth-child(1) > a')).click();
-        const text = await element(by.css('h1#ngzone')).getText();
-        expect(text).toBe('NgZone');
+        const text = await browser.getCurrentUrl();
+        expect(text).toEqual('https://angular.io/api/core/NgZone');
     });
 });
